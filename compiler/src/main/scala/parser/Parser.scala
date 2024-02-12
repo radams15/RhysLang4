c193859679610d86a5a52848196cfe16ad66bd0a
Rhys Adams <rhys@therhys.co.uk>
2024-02-12 20:05:59 +0000

    Add some tokens.

--- compiler/src/main/scala/parser/Parser.scala
+++ compiler/src/main/scala/parser/Parser.scala
@@ -5,8 +5,10 @@ import scala.collection.mutable.ListBuffer
 import scala.reflect.ClassTag
 import scala.util.matching.Regex
 
+import node.*
+
 class Parser[T](_parse: Source => ParseResult[T]) {
-  def parse: Source => ParseResult[T] = _parse
+  val parse: Source => ParseResult[T] = _parse
 
   def or(parser: Parser[T]): Parser[T] =
     new Parser(source => {
@@ -24,7 +26,7 @@ class Parser[T](_parse: Source => ParseResult[T]) {
         case res if res != null =>
           res.getValue match {
             case Some(n) => callback(n).parse(source)
-            case null => throw Exception("Undefined in bind")
+            case null | None => throw Exception("Undefined in bind")
           }
         case null => null
       }
@@ -87,9 +89,43 @@ object Parser {
   def maybe[U](parser: Parser[U]): Parser[U] =
     parser.or(constant(null))
 
-  def pair: Parser[Array[String]] =
-    regexp("[0-9] +".r).bind((first) =>
-      regexp(", ".r).and(
-        regexp("[0-9] +".r).map((second) =>
-          Array(first, second))))
+  val token: Regex => Parser[String] =
+    pattern => {
+      regexp(pattern)
+        .bind(value =>
+          ignored.and(constant(Some(value))))
+    }
+
+  val whitespace = regexp("[ \n\r\t]+".r)
+  val comments = regexp("//.*".r)
+  val ignored = zeroOrMore(whitespace.or(comments))
+
+  val FUNCTION = token("function\b".r)
+  val IF = token("if\b".r)
+  val ELSE = token("else\b".r)
+  val RETURN = token("return\b".r)
+  val VAR = token("var\b".r)
+  val WHILE = token("while\b".r)
+
+  val COMMA = token(",".r)
+  val SEMICOLON = token(";".r)
+  val LEFT_PAREN = token("\\(".r)
+  val RIGHT_PAREN = token("\\)".r)
+  val LEFT_BRACE = token("\\[".r)
+  val RIGHT_BRACE = token("]".r)
+
+  val NUMBER = token("[0-9]+".r)
+    .map(digits => new Number(Integer.parseInt(digits)))
+
+  val ID = token("[a-zA-Z_][a-zA-Z0-9_]*".r)
+
+  val id = ID.map(x => new Id(x))
+
+  val NOT = token("!".r).map(_ => classOf[Not])
+  val EQUAL = token("==".r).map(_ => classOf[Equal])
+  val NOT_EQUAL = token("!=".r).map(_ => classOf[NotEqual])
+  val PLUS = token("\\+".r).map(_ => classOf[Add])
+  val MINUS = token("-".r).map(_ => classOf[Subtract])
+  val STAR = token("\\*".r).map(_ => classOf[Multiply])
+  val SLASH = token("/".r).map(_ => classOf[Divide])
 }
